d860e4beb2b116835f1b674f478caf325a962938
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-05-19 19:08:52 +0100

    Started styling the articles

--- docs/index.js
+++ docs/index.js
@@ -34,11 +34,31 @@ const register = () => {
   customElements.define('news-list', NewsList);
 };
 
+const style = `
+  <style>
+    .article-container {
+      max-width: 22rem;
+      margin: 0 auto;
+      width: 95%;
+      background: black;
+    }
+  </style>
+`;
+
+const content = `
+  <div class='article-container'>
+    <p>article</p>
+  </div>
+`;
+
 class NewsArticle extends HTMLElement {
   constructor() {
     super();
 
-    // this.innerHTML('test');
+    this._shadowRoot = this.attachShadow({ mode: 'closed' });
+    this._shadowRoot.innerHTML = style + content;
+
+    console.log(this);
   }
 }
 
