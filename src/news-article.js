7633b4f903e10afbffe80e135c7598fcf23f80cc
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-05-19 21:02:37 +0100

    thumbnail now in place

--- src/news-article.js
+++ src/news-article.js
@@ -4,14 +4,34 @@ const style = `
       max-width: 22rem;
       margin: 0 auto;
       width: 95%;
-      background: black;
+      display: flex;
+      flex-direction: row;
+      margin-bottom: .5rem;
+    }
+
+    .thumbnail-container {
+      width: 11rem;
+      height: 6.1875rem;
+      background: #A8A8A8;
+    }
+
+    .content-container {
+      flex-grow: 1;
     }
   </style>
 `;
 
 const content = `
   <div class='article-container'>
-    <p>article</p>
+
+    <div class='thumbnail-container'>
+      <img class='thumbnail' >
+    </div>
+    
+    <div class='content-container'>
+      <p>article</p>
+    </div>
+
   </div>
 `;
 
